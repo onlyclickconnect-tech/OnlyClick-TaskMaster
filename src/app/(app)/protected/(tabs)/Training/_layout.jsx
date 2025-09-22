90bb6ef7b7751e9bfc54f80d5dbece77165e515c
Nilesh Bera <nileshbera258@gmail.com>
2025-09-23 01:27:44 +0530

    training videos

--- src/app/(app)/protected/(tabs)/Training/_layout.jsx
+++ src/app/(app)/protected/(tabs)/Training/_layout.jsx
@@ -1,4 +1,10 @@
-import { Slot } from "expo-router";
-export default function HomeLayout() {
-  return <Slot />;
+import { Stack } from "expo-router";
+
+export default function TrainingLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="video-player" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
