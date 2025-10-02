ff9f24c694e9c56a5b3d5ed7bf05b993f8bbcbc2
Amar <onlyaboutcoc@gmail.com>
2025-10-02 13:47:40 +0530

    image: latest fix for android 15

--- app.config.js
+++ app.config.js
@@ -49,15 +49,12 @@ export default {
     extra: {
       router: {},
       eas: {
-        "projectId": "5e84b62b-c0c2-4bbc-9ab2-6b9b124b8ae1"
+        projectId: "fb41cc19-ffd6-495c-a77a-94f9b77a86db"
       },
       // Environment variables for the app
-      expoPublicSupabaseUrl:
-        process.env.EXPO_PUBLIC_SUPABASE_URL,
-      expoPublicSupabaseAnonKey:
-        process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-      expoPublicApiUrl:
-        process.env.EXPO_PUBLIC_API_URL
+      expoPublicSupabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      expoPublicSupabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+      expoPublicApiUrl: process.env.EXPO_PUBLIC_API_URL,
     },
   },
 };
